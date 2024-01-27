9f7fa5bb6b4d86e19901153e5c1df9a6e8464a90
Tania <taniairgasheva@ya.ru>
2024-01-28 01:44:02 +0300

    makeFriendsList

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,35 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let ul = document.createElement("ul");
+
+  friends.forEach((f) => {
+    let li = document.createElement("li");
+    li.innerHTML = `${f.firstName} ${f.lastName}`;
+    ul.appendChild(li);
+  });
+
+  return ul;
+
+  //FOR-LOOP:
+
+  // for (let i = 0; i < friends.length; i++) {
+  //   let li = document.createElement("li");
+  //   li.innerHTML = `${friends[i].firstName} ${friends[i].lastName}`;
+  //   ul.appendChild(li);
+  // }
+
+  //FOR...OF:
+
+  // for (let i of friends) {
+  //   let li = document.createElement("li");
+  //   li.innerHTML = `${i.firstName} ${i.lastName}`;
+  //   ul.appendChild(li);
+  // }
+
+  //MAP
+
+  // friends.map((f) => {
+  //   li = document.createElement("li");
+  //   li.innerHTML = `${f.firstName} ${f.lastName}`;
+  //   ul.appendChild(li);
+  // });
 }

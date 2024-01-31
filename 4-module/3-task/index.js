e21c0f121cc18b1fcd2b771228eba36e128e565d
Tania <taniairgasheva@ya.ru>
2024-01-31 22:53:28 +0300

    highlightFunction

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
-  // ваш код...
+  let rowsList = table.querySelectorAll("tr");
+  for (let row of rowsList) {
+    let status = row.children[3].dataset.available; //'true', 'false', 'undefined'
+    let gender = row.children[2];
+    let age = row.children[1];
+
+    //Добавление класса и атрибута
+    if (!status) row.hidden = true;
+    status === "true"
+      ? row.classList.add("available")
+      : row.classList.add("unavailable");
+
+    //Добавление класса в зависимости от гендера
+    row.classList.add(gender.innerText === "m" ? "male" : "female");
+
+    //Проверка возраста. || находит первое истинное.
+    age.innerText > 18 ||
+      row.setAttribute("style", "text-decoration: line-through");
+  }
 }

c6c98b9fbbf1414e4e05da15abb9efa34b2c1e8a
Tania Irgasheva <taniairgasheva@ya.ru>
2024-02-01 16:03:18 +0300

    Updated

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,9 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector(".toggle-text-button");
+  let text = document.getElementById("text");
+
+  button.onclick = () => {
+    text.classList.toggle("toggle-text-button");
+    text.hidden = !text.hidden;
+  };
 }
